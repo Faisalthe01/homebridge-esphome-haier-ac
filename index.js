41d69f66e1398de56625e14dda664d7938b442a3
Faisal <62862684+Faisalthe01@users.noreply.github.com>
2025-08-17 17:07:20 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,38 @@
 const ESPHome = require('./lib/esphome');
 const PLUGIN_NAME = 'homebridge-esphome-haier-ac';
-const PLATFORM_NAME = 'ESPHomeAC';
+const PLATFORM_NAME = 'HaierAC'; // Unique platform name
 
 module.exports = (api) => {
-  api.registerPlatform(PLUGIN_NAME, PLATFORM_NAME, ESPHomeAC);
+    api.registerPlatform(PLUGIN_NAME, PLATFORM_NAME, ESPHomeAC);
 };
 
 class ESPHomeAC {
-  constructor(log, config, api) {
-    this.api = api;
-    this.log = log;
+    constructor(log, config, api) {
+        this.api = api;
+        this.log = log;
 
-    this.accessories = [];
-    this.esphomeDevices = {};
-    this.PLUGIN_NAME = PLUGIN_NAME;
-    this.PLATFORM_NAME = PLATFORM_NAME;
-    this.name = config.name || PLATFORM_NAME;
-    this.devices = config.devices || [];
-    this.debug = config.debug || false;
+        this.accessories = [];
+        this.esphomeDevices = {};
+        this.PLUGIN_NAME = PLUGIN_NAME;
+        this.PLATFORM_NAME = PLATFORM_NAME;
+        this.name = config.name || PLATFORM_NAME;
+        this.devices = config.devices || [];
+        this.debug = config.debug || false;
 
-    // debug helper
-    this.log.easyDebug = (...content) => {
-      if (this.debug) {
-        this.log(`${content.join(' ')}`);
-      } else {
-        this.log.debug(`${content.join(' ')}`);
-      }
-    };
+        // Debug logger
+        this.log.easyDebug = (...content) => {
+            if (this.debug) {
+                this.log(content.join(' '));
+            } else {
+                this.log.debug(content.join(' '));
+            }
+        };
 
-    // Wait for Homebridge to finish launching
-    this.api.on('didFinishLaunching', () => ESPHome.init.call(this));
-  }
+        this.api.on('didFinishLaunching', ESPHome.init.bind(this));
+    }
 
-  configureAccessory(accessory) {
-    this.log.easyDebug(`Found Cached Accessory: ${accessory.displayName} (${accessory.context.deviceId})`);
-    this.accessories.push(accessory);
-  }
+    configureAccessory(accessory) {
+        this.log.easyDebug(`Found Cached Accessory: ${accessory.displayName} (${accessory.context.deviceId})`);
+        this.accessories.push(accessory);
+    }
 }
